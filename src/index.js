f48e978518d1b0cd82403b1712662a8caa2b7965
Dima <sobolevskii.dmit@gmail.com>
2023-02-19 01:20:23 +0400

    submit brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const openBrackets = []
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0])
+  }
+  console.log('openBrackets', openBrackets)
+
+  const bracketsPair = {}
+  const identicalPairs = []
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+      identicalPairs.push(bracketsConfig[i][0])
+    }
+
+    bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0]
+  }
+  console.log('bracketsPair', bracketsPair)
+  console.log('identicalPairs', identicalPairs)
+
+
+  let stack = []
+
+  for (let k = 0; k < str.length; k++) {
+    let currentSymbol = str[k]
+    let topElement = stack[stack.length - 1]
+
+    if (identicalPairs.includes(currentSymbol) && currentSymbol === topElement) {
+      stack.pop()
+    } else {
+      if (openBrackets.includes(currentSymbol)) {
+        stack.push(currentSymbol)
+      } else {
+        
+        if (stack.length === 0) {
+          return false
+        }
+  
+        
+  
+        if (bracketsPair[currentSymbol] === topElement) {
+          stack.pop()
+        } else {
+          return false
+        }
+      }
+    } 
+
+  }
+
+  return stack.length === 0;
 }
